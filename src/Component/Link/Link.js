a4b48b41388a0015400db0e469e1cd7f6f112d18
Richardjackson-web <richardjackson556@gmail>
2022-10-28 22:59:32 +0100

    I added the title and subtext, alongside the share button.

--- src/Component/Link/Link.js
+++ src/Component/Link/Link.js
@@ -11,19 +11,19 @@ function Link() {
                 Zuri Team 
             </a>
             <br /><br/><br/>
-            <a href="http://books.zuri.team" id="books">
+            <a href="http://books.zuri.team" id="books" title="This is where you will find books about design and coding.">
                 Zuri Books
             </a>
             <br /><br/><br/>
-            <a href="https://books.zuri.team/" id="book__python">
+            <a href="https://books.zuri.team/python-for-beginners?ref_id=RichardJackson-web" id="book__python" title="Have issues understanding python? Let's help you get it by the tail.">
                 Python Books
             </a>
             <br /><br/><br/>
-            <a href="https://background.zuri.team/" id="pitch">
+            <a href="https://background.zuri.team/" id="pitch" title="When hiring a coder for a job you need to know who it is exactly you are hiring. Do a background check here.">
                 Background Check for Coders
             </a>
             <br /><br/><br/>
-            <a href="https://books.zuri.team/design-rules" id="book__design">
+            <a href="https://books.zuri.team/design-rules" id="book__design" title="It's one thing to design and another to make appealing designs. Check out this rule book.">
                 Design Books
             </a>
         </div>
--- src/Component/Profile/Profile.js
+++ src/Component/Profile/Profile.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './Profile.css';
 import Image from '../../Image/Profile_Image.jpg';
+import Share from '../../Image/share.png';
+import Mobile from '../../Image/mobile-share.png';
 
 function Profile() {
   return (
-    <div>
+    <div className="pro">
+        <img className="share" src={Share}/>
+        <img src={Mobile} className="mobile-share"/>
         <img id="profile__img" src={Image} />
         <h2 id="twitter">@i_am_richaard</h2>
         {/* <h2 id="slack">RichardJackson-web</h2> */}
