1de6ef0ad1e6dc0a8a861e8bab18507399c39fdf
Richardjackson-web <richardjackson556@gmail>
2022-10-28 02:24:21 +0100

    I'm done with the hard coding, all that's left is its responsiveness.

--- src/Component/Link/Link.js
+++ src/Component/Link/Link.js
@@ -5,29 +5,27 @@ function Link() {
   return (
     <div>
         <div className="main-btn">
-            <button className="link-button">
-                Twitter
-            </button> 
+            <a className="link-button">Twitter Link</a> 
             <br /><br/>
-            <button className="link-button">
-                Block level button
-            </button>
+            <a href="https://training.zuri.team/" id="btn__zuri" className="link-button">
+                Zuri Team 
+            </a>
             <br /><br/>
-            <button className="link-button">
-                Block level button
-            </button>
+            <a href="http://books.zuri.team" id="books" className="link-button">
+                Zuri Books
+            </a>
             <br /><br/>
-            <button className="link-button">
-                Block level button
-            </button>
+            <a href="https://books.zuri.team/" id="book__python" className="link-button">
+                Python Books
+            </a>
             <br /><br/>
-            <button className="link-button">
-                Block level button
-            </button>
+            <a href="https://background.zuri.team/" id="pitch" className="link-button">
+                Background Check for Coders
+            </a>
             <br /><br/>
-            <button className="link-button">
-                Block level button
-            </button>
+            <a href="https://books.zuri.team/design-rules" id="book__design" className="link-button">
+                Design Books
+            </a>
         </div>
     </div>
   )
