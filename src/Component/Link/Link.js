aebd7cfad2ef88c2adc8d92caeaf68d6842a07ad
Richardjackson-web <richardjackson556@gmail>
2022-10-28 03:08:38 +0100

    Finished with the responsiveness across different screen sizes

--- src/Component/Link/Link.js
+++ src/Component/Link/Link.js
@@ -5,25 +5,25 @@ function Link() {
   return (
     <div>
         <div className="main-btn">
-            <a className="link-button">Twitter Link</a> 
-            <br /><br/>
-            <a href="https://training.zuri.team/" id="btn__zuri" className="link-button">
+            <a href="https://twitter.com/i_am_richaard" className="link-button">Twitter Link</a> 
+            <br /><br/><br/>
+            <a href="https://training.zuri.team/" id="btn__zuri">
                 Zuri Team 
             </a>
-            <br /><br/>
-            <a href="http://books.zuri.team" id="books" className="link-button">
+            <br /><br/><br/>
+            <a href="http://books.zuri.team" id="books">
                 Zuri Books
             </a>
-            <br /><br/>
-            <a href="https://books.zuri.team/" id="book__python" className="link-button">
+            <br /><br/><br/>
+            <a href="https://books.zuri.team/" id="book__python">
                 Python Books
             </a>
-            <br /><br/>
-            <a href="https://background.zuri.team/" id="pitch" className="link-button">
+            <br /><br/><br/>
+            <a href="https://background.zuri.team/" id="pitch">
                 Background Check for Coders
             </a>
-            <br /><br/>
-            <a href="https://books.zuri.team/design-rules" id="book__design" className="link-button">
+            <br /><br/><br/>
+            <a href="https://books.zuri.team/design-rules" id="book__design">
                 Design Books
             </a>
         </div>
