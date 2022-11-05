b9b5dd9b87da002f49226fbff0a75059ceb2a4d3
Richardjackson-web <richardjackson556@gmail>
2022-11-05 13:00:29 +0100

    feat:I coded the form in the contact component

--- src/Pages/Contact/Contact.js
+++ src/Pages/Contact/Contact.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import './Contact.css';
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
+import Footer from '../../Component/Footer/Footer';
 
 function Contact() {
   return (
@@ -14,14 +20,45 @@ function Contact() {
             <Form>
             <Row>
         <Col>
+            <Form.Label>First name</Form.Label>
           <Form.Control placeholder="First name" />
         </Col>
         <Col>
+        <Form.Label>Last name</Form.Label>
           <Form.Control placeholder="Last name" />
         </Col>
-      </Row>
+        </Row>
+        <Row>
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type="email" placeholder="name@example.com" />
+        </Row>
+        <Row>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+            <Form.Label>Message</Form.Label>
+            <Form.Control as="textarea" rows={3} placeholder="Send me a message and I'll reply you as soon as possible..." />
+            </Form.Group>
+        </Row>
+        <Row>
+        {['checkbox'].map((type) => (
+        <div key={`inline-${type}`} className="mb-3">
+          <Form.Check
+            inline
+            label="You agree to providing your data to {name} who may contact you."
+            name="group1"
+            type={type}
+            id={`inline-${type}-1`}
+          />
+        </div>
+      ))}
+        </Row>
     </Form>
         </div>
+        <div className="d-grid gap-2">
+      <Button variant="primary" size="lg">
+        Block level button
+      </Button>
+      </div>
+      <Footer/>
     </div>
   )
 }
